80870f191044f7af1c1caee3b18ee7c23e2149ab
TK Soh <teekaysoh@gmail.com>
2023-02-15 11:15:43 +0800

    restore formatting of FOOD_DATA variable

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,32 +1,57 @@
 // ignore: constant_identifier_names
 const FOOD_DATA = [
-  {"name": "Burger", "brand": "Hawkers", "price": 2.99, "image": "burger.png"},
+  {
+    "name": "Burger",
+    "brand": "Hawkers",
+    "price": 2.99,
+    "image": "burger.png",
+  },
   {
     "name": "Cheese Dip",
     "brand": "Hawkers",
     "price": 4.99,
-    "image": "cheese_dip.png"
+    "image": "cheese_dip.png",
   },
-  {"name": "Cola", "brand": "Mcdonald", "price": 1.49, "image": "cola.png"},
-  {"name": "Fries", "brand": "Mcdonald", "price": 2.99, "image": "fries.png"},
+  {
+    "name": "Cola",
+    "brand": "Mcdonald",
+    "price": 1.49,
+    "image": "cola.png",
+  },
+  {
+    "name": "Fries",
+    "brand": "Mcdonald",
+    "price": 2.99,
+    "image": "fries.png",
+  },
   {
     "name": "Ice Cream",
     "brand": "Ben & Jerry's",
     "price": 9.49,
-    "image": "ice_cream.png"
+    "image": "ice_cream.png",
   },
   {
     "name": "Noodles",
     "brand": "Hawkers",
     "price": 4.49,
-    "image": "noodles.png"
+    "image": "noodles.png",
   },
-  {"name": "Pizza", "brand": "Dominos", "price": 17.99, "image": "pizza.png"},
+  {
+    "name": "Pizza",
+    "brand": "Dominos",
+    "price": 17.99,
+    "image": "pizza.png",
+  },
   {
     "name": "Sandwich",
     "brand": "Hawkers",
     "price": 2.99,
-    "image": "sandwich.png"
+    "image": "sandwich.png",
   },
-  {"name": "Wrap", "brand": "Subway", "price": 6.99, "image": "wrap.png"}
+  {
+    "name": "Wrap",
+    "brand": "Subway",
+    "price": 6.99,
+    "image": "wrap.png",
+  },
 ];
